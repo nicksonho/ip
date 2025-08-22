412be0529a6753b34b7d7fd1793b1d15f4dea658
nickson <nicksonho7@gmail.com>
2025-08-22 13:52:31 +0800

    gave chatbot name and created initial skeletal version

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# NixChats User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/NixChats.java
@@ -0,0 +1,19 @@
+public class NixChats {
+    public static void main(String[] args) {
+        greet();
+        exit();
+    }
+
+    public static void greet() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm NixChats");
+        System.out.println("What can i do for you?");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
+
