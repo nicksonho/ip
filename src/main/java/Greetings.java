4883735e1cdea7e484fb0a167e9e5c6594c01492
nickson <nicksonho7@gmail.com>
2025-08-22 13:59:11 +0800

    moved greetings to separate class

---
+++ src/main/java/Greetings.java
@@ -0,0 +1,13 @@
+public class Greetings {
+    public static void greet() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm NixChats");
+        System.out.println("What can i do for you?");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
--- src/main/java/NixChats.java
+++ src/main/java/NixChats.java
@@ -1,19 +1,9 @@
 public class NixChats {
     public static void main(String[] args) {
-        greet();
-        exit();
+        Greetings.greet();
+        Greetings.exit();
     }
 
-    public static void greet() {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm NixChats");
-        System.out.println("What can i do for you?");
-        System.out.println("____________________________________________________________");
-    }
 
-    public static void exit() {
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
-    }
 }
 
