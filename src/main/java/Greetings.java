2c3a7557dc30314be3f7a4666686b30eadfb0ce4
nickson <nicksonho7@gmail.com>
2025-08-22 14:26:44 +0800

    add, list functionality

--- src/main/java/Greetings.java
+++ src/main/java/Greetings.java
@@ -1,13 +1,19 @@
 public class Greetings {
-    public static void greet() {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm NixChats");
-        System.out.println("What can i do for you?");
+
+    public static void divider() {
         System.out.println("____________________________________________________________");
     }
 
+    public static void greet() {
+        divider();
+        System.out.println("Hello! I'm NixChats");
+        System.out.println("What can i do for you?");
+        divider();
+    }
+
     public static void exit() {
+        divider();
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        divider();
     }
 }
--- src/main/java/NixChats.java
+++ src/main/java/NixChats.java
@@ -1,19 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class NixChats {
     public static void main(String[] args) {
         Greetings.greet();
         Scanner sc = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
         while (true) {
             System.out.print("You: ");
             String input = sc.nextLine();
             if (input.equalsIgnoreCase("bye")) {
                 Greetings.exit();
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                Greetings.divider();
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(i+1 + ": " + list.get(i));
+                }
+                Greetings.divider();
+            } else {
+                list.add(input);
+                Greetings.divider();
+                System.out.println("added: " + input);
+                Greetings.divider();
             }
-            System.out.println("____________________________________________________________");
-            System.out.println("Echo: " + input);
-            System.out.println("____________________________________________________________");
         }
         sc.close();
     }
