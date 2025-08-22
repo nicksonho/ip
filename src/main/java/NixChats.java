b0b80f68a337e529036f8faa5e1d4c51ed40e722
nickson <nicksonho7@gmail.com>
2025-08-22 14:10:46 +0800

    added echo functionality

--- src/main/java/NixChats.java
+++ src/main/java/NixChats.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class NixChats {
     public static void main(String[] args) {
         Greetings.greet();
-        Greetings.exit();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("You: ");
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                Greetings.exit();
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println("Echo: " + input);
+            System.out.println("____________________________________________________________");
+        }
+        sc.close();
     }
 
 
